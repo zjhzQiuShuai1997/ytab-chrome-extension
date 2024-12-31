4fd2c747ec0b903e3925232fd200bb35257fb9f6
ssd-dev <l17610925150@163.com>
2024-12-31 11:35:05 +0800

    feat: 添加 Vue 3 + TypeScript + Vite 项目模板
    - 新增 ESLint、Prettier 配置文件，规范代码格式
    - 添加背景脚本、内容脚本和 inpage 脚本文件
    - 创建 README 文件，提供项目说明和使用指南
    - 设置 TypeScript、Vite 和其他开发依赖

---
+++ vite.config.ts
@@ -0,0 +1,61 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import webExtension from '@samrum/vite-plugin-web-extension';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
+import { resolve } from 'path';
+import manifest from './src/manifest';
+// https://vitejs.dev/config/
+export default ({ mode, command }) => {
+  console.log(mode, command);
+  return defineConfig({
+    root: './src/',
+    base: '/',
+    envDir: '../',
+    publicDir: '../public',
+    plugins: [
+      vue(),
+      webExtension({
+        manifest: {
+          ...manifest
+        }
+      })
+    ],
+    define: {
+      'process.env': {}
+    },
+    server: {
+      host: '0.0.0.0',
+      port: 30,
+      strictPort: true,
+      open: true
+    },
+    css: {
+      devSourcemap: true
+    },
+    rollupOptions: {
+      plugins: [nodePolyfills()]
+    },
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src')
+      }
+    },
+    build: {
+      outDir: '../dist',
+      emptyOutDir: true,
+      cssCodeSplit: true,
+      sourcemap: true,
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, './src/index.html'),
+          options: resolve(__dirname, './src/options.html')
+        },
+        output: {
+          chunkFileNames: 'static/js/[name]-[hash].js',
+          entryFileNames: 'static/js/[name]-[hash].js',
+          assetFileNames: 'static/[ext]/name-[hash].[ext]'
+        }
+      }
+    }
+  });
+};
